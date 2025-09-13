e3b11d6a807815b55c5042393f70aab94cecc20a
madscream69 <ww010405@gmail.com>
2025-09-13 21:12:04 +0300

    firest commit and start structure

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./pages/Home/Home.tsx";
+import NotFound from "./pages/NotFound/NotFound.tsx";
+import Layout from "./components/Layout/Layout.tsx";
+import About from "./pages/About/About.tsx";
+//import styles from './App.module.scss'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      /*{ path: "category/:categoryId", element: <Category /> },
+      { path: "product/:productId", element: <ProductDetails /> },*/
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
+
+function App() {
+
+  return <RouterProvider router={router} />;
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+
+
+function Header() {
+    return (
+        <header>
+            <p>Header</p>
+        </header>
+    );
+}
+
+export default Header;
